7888a39bceee1a28ac0f2fd9d35fe44f1dabccf8
Luke Schlessinger <lmschless@gmail.com>
2020-09-04 15:51:09 -0700

    moved check if valid ID logic to a new middleware instead of being inside of each route handler.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,6 +7,20 @@ const __dirname = path.resolve();
 // Read data at top level so it only gets executed once
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+// use param middleware to check if id is valid instead of checking inside every function.
+
+const checkID = (req, res, next, val) => {
+  console.log(`Tour ID is: ${val}`);
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  next(); // only runs if the previous check is successful
+};
+
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -25,12 +39,7 @@ const getTour = (req, res) => {
   const tour = tours.find((tour) => tour.id === id);
   // add check to throw error if id is > length of array or if  the find method cannot find the id.
   // if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -60,12 +69,6 @@ const createTour = (req, res) => {
 };
 
 const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -75,16 +78,10 @@ const updateTour = (req, res) => {
 };
 
 const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
   res.status(204).json({
     status: 'success',
     data: null,
   });
 };
 
-export default { getAllTours, createTour, getTour, deleteTour, updateTour };
+export default { checkID, getAllTours, createTour, getTour, deleteTour, updateTour };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import controller from '../controllers/tourController.js';
-const { getAllTours, createTour, getTour, deleteTour, updateTour } = controller;
+const { getAllTours, createTour, getTour, deleteTour, updateTour, checkID } = controller;
 
 const router = express.Router();
+
+// using imported middleware from tourController to check if ID is valid.
+router.param('id', checkID);
+
 router.route('/').get(getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).delete(deleteTour).patch(updateTour);
