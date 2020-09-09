54446545238e0d10d999841785870a851f72d409
Luke Schlessinger <lmschless@gmail.com>
2020-09-08 21:35:25 -0700

    added mongo and mongoose to project. Added mongo atlas db params to config file. Successfully connected to db.

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 // eslint-disable-next-line import/extensions
 import app from './app.js';
 
 dotenv.config({ path: './config.env' }); // have to import dotenv before app
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// pass connection string and object with options into Mongoose
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB Connection Successful!');
+  })
+  .catch((error) => console.log(error));
 
 //// START SERVER
 const port = process.env.PORT || 3000;
